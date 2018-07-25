8fe2612f74ca9bcc492b3706e5f08f24062d8c20
jorgevalencialopez <41311195+jorgevalencialopez@users.noreply.github.com>
2018-07-25 09:12:56 -0700

    Update example3.2.m

--- ch03_matlab_ex/example3.2.m
+++ ch03_matlab_ex/example3.2.m
@@ -4,8 +4,8 @@
 %  Chris J. Solomon and Toby P. Breckon, Wiley-Blackwell, 2010
 %  ISBN: 0470844736, DOI:10.1002/9780470689776, http://www.fundipbook.com
 %
-A=imread('firstimage.jpg');  % Read in 1st image
-B=imread('secondimage.jpg'); % Read in 2nd image
+A=imread('cola1.png');  % Read in 1st image
+B=imread('cola2.png'); % Read in 2nd image
 
 subplot(1,3,1), imshow(A); % Display 1st image 
 subplot(1,3,2), imshow(B); % Display 2nd image
